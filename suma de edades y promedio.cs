481c56d2e324922b4efc765d41f971ff953ce7af
JuanRg68 <juanrugeles97@gmail.com>
2025-04-22 14:56:14 -0500

    Update and rename eyp.sln to suma de edades y promedio.cs

---
+++ suma de edades y promedio.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("¿Cuántas edades vas a ingresar? ");
+int cantidad = Convert.ToInt32(Console.ReadLine());
+int suma = 0;
+
+
+for (int i = 1; i <= cantidad; i++)
+{
+    Console.Write("Edad #" + i + ": ");
+    int edad = Convert.ToInt32(Console.ReadLine());
+    suma += edad;
+}
+
+double promedio = (double)suma / cantidad;
+
+Console.WriteLine("\nSuma total de edades: " + suma);
+Console.WriteLine("Promedio de edades: " + promedio);
